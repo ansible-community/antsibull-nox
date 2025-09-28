fcecfc4b073764f55d77deeb08ed52b64cd62603
Felix Fontein <felix@fontein.de>
2025-09-28 14:46:21 +0200

    Add basic change detection for docs-check (#130)
    * Add basic change detection for docs-check.
    
    * Linting.

--- src/antsibull_nox/sessions/docs_check.py
+++ src/antsibull_nox/sessions/docs_check.py
@@ -15,6 +15,9 @@ from pathlib import Path
 
 import nox
 
+from ..cd import (
+    get_changes,
+)
 from ..paths import (
     list_all_files,
 )
@@ -47,6 +50,29 @@ def find_extra_docs_rst_files() -> list[Path]:
         for file in all_files
         if file.is_relative_to(extra_docs_dir) and file.name.lower().endswith((".rst"))
     ]
+
+
+def file_of_interest(file: Path) -> bool:
+    """
+    Determine whether the file is relevant for the docs build.
+    """
+    if file.is_relative_to("docs/docsite"):
+        return True
+    if file in (Path("meta/runtime.yml"), Path("galaxy.yml")):
+        return True
+    if file.is_relative_to("plugins"):
+        if file.is_relative_to("plugins/module_utils"):
+            return False
+        if file.is_relative_to("plugins/plugin_utils"):
+            return False
+        # Other Python and YAML plugin/ files often contain documentation.
+        # Other files do not.
+        return file.suffix in (".py", ".yml", ".yaml")
+    if file.is_relative_to("roles"):
+        # For roles, only files in <role>/meta/ are of interest for documentation.
+        return file.match("roles/*/meta/*")
+    # So far, no other files are of interest for documentation.
+    return False
 
 
 def add_docs_check(
@@ -95,11 +121,19 @@ def add_docs_check(
                 "codeblocks_allow_without_type": codeblocks_allow_without_type,
                 "codeblocks_allow_literal_blocks": codeblocks_allow_literal_blocks,
             },
+            with_cd=True,
         )
 
     def execute_antsibull_docs(
         session: nox.Session, prepared_collections: CollectionSetup
     ) -> None:
+        changes = get_changes()
+        if changes is not None:
+            if not any(file_of_interest(file) for file in changes):
+                session.warn(
+                    "Skip antsibull-docs lint-collection-docs (no files to process)"
+                )
+                return
         antsibull_docs_version = get_package_version(session, "antsibull-docs")
         if antsibull_docs_version is not None:
             session.log(f"Detected antsibull-docs version {antsibull_docs_version}")
