1ed79420df2a312028791afbcfbecb68900d4cd6
Felix Fontein <felix@fontein.de>
2026-06-30 19:03:09 +0200

    Adjust to latest milestone bump.

--- tests/needs-network/test_ansible_core.py
+++ tests/needs-network/test_ansible_core.py
@@ -24,7 +24,7 @@ _EOL_ANSIBLE_BRANCH_TEST_URL = (
 
 # You can get this value from https://github.com/ansible/ansible/commits/milestone.
 # One this changes, information src/antsibull_nox/ansible.py might need to be updated.
-_MILESTONE_LAST_COMMIT = "a496819b4dc9c95524f5666e78ed2110ec1c76dc"
+_MILESTONE_LAST_COMMIT = "b475463834458781505d7d9e3344da40251f48dd"
 
 
 def get_branch_version(branch_name: str) -> Version:
