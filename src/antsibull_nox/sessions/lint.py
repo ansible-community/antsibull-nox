ae4b767e51ca3cf4e7bcb0e6c2d881befefd8494
Felix Fontein <felix@fontein.de>
2026-08-08 22:10:35 +0200

    Aborted attempt to improve ruff format --check output.

--- src/antsibull_nox/sessions/lint.py
+++ src/antsibull_nox/sessions/lint.py
@@ -390,10 +390,10 @@ def _execute_ruff_format_for(
     ruff_format_config: str | os.PathLike | None,
     what_for: str = "",
     reporter: PartReporter,  # pylint: disable=unused-argument
-) -> None:
+) -> list[Message]:
     if not files:
         session.warn(f"Skipping ruff format{what_for} (no files to process)")
-        return
+        return []
     command: list[str] = [
         "ruff",
         "format",
@@ -402,14 +402,37 @@ def _execute_ruff_format_for(
         command.append("--check")
     if ruff_format_config is not None:
         command.extend(["--config", str(ruff_format_config)])
+    # Disabled because of https://github.com/astral-sh/ruff/issues/27602, see below.
+    # if run_check:
+    #     command.append("--output-format=json")
     command.extend(session.posargs)
     command.extend(str(file) for file in files)
+
+    # Unfortunately this doesn't work, since 'ruff format --check' uses exit code 2
+    # when encountering syntax errors in files. Until this behavior of ruff is fixed,
+    # we cannot provide better output.
+    # if run_check:
+    #     # https://docs.astral.sh/ruff/linter/#exit-codes
+    #     output = session.run(*command, silent=True, success_codes=[0, 1])
+    #     return (
+    #              pylint: disable-next=fixme
+    #              TODO: does parse_ruff_check_errors() actually work well
+    #                    for 'ruff format --check' output?
+    #         parse_ruff_check_errors(
+    #             source_path=Path.cwd(),
+    #             output=output,
+    #         )
+    #         if output
+    #         else []
+    #     )
+
     session.run(*command)
     # pylint: disable-next=fixme
     # TODO: use https://github.com/wntrblm/nox/pull/1124 to include error output
     # pylint: disable-next=fixme
     # TODO: find out whether ruff format can output error information somehow else
     #       than stdout/stderr
+    return []
 
 
 def _execute_ruff_format(
@@ -430,32 +453,46 @@ def _execute_ruff_format(
         config=ruff_format_config,
         config_modules=ruff_format_modules_config or ruff_format_config,
     )
+    messages = []
     if files is not None:
-        _execute_ruff_format_for(
-            session,
-            run_check=run_check,
-            files=files,
-            ruff_format_config=ruff_format_config,
-            reporter=reporter,
+        messages.extend(
+            _execute_ruff_format_for(
+                session,
+                run_check=run_check,
+                files=files,
+                ruff_format_config=ruff_format_config,
+                reporter=reporter,
+            )
         )
     if files_modules is not None:
-        _execute_ruff_format_for(
-            session,
-            run_check=run_check,
-            files=files_modules,
-            ruff_format_config=ruff_format_modules_config or ruff_format_config,
-            what_for=" for modules and module utils",
-            reporter=reporter,
+        messages.extend(
+            _execute_ruff_format_for(
+                session,
+                run_check=run_check,
+                files=files_modules,
+                ruff_format_config=ruff_format_modules_config or ruff_format_config,
+                what_for=" for modules and module utils",
+                reporter=reporter,
+            )
         )
     if files_other is not None:
-        _execute_ruff_format_for(
-            session,
-            run_check=run_check,
-            files=files_other,
-            ruff_format_config=ruff_format_config,
-            what_for=" for other files",
-            reporter=reporter,
+        messages.extend(
+            _execute_ruff_format_for(
+                session,
+                run_check=run_check,
+                files=files_other,
+                ruff_format_config=ruff_format_config,
+                what_for=" for other files",
+                reporter=reporter,
+            )
         )
+
+    reporter.report_messages(messages)
+    print_messages(
+        session=session,
+        messages=messages,
+        fail_msg="Ruff format check failed",
+    )
 
 
 def _execute_ruff_autofix_for(
