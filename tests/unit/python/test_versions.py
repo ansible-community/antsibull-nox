cf5caaaa1d159bf9a071d6c7f12ca5a459b012c1
Felix Fontein <felix@fontein.de>
2026-08-11 12:54:33 +0200

    Add tests.

--- tests/unit/python/test_versions.py
+++ tests/unit/python/test_versions.py
@@ -6,12 +6,20 @@
 
 from __future__ import annotations
 
+import sys
 from pathlib import Path
 
-from antsibull_nox.python.versions import get_installed_python_versions
+from antsibull_nox.python.versions import _LATEST_PYTHON_VERSION, get_installed_python_versions, get_recent_python_version
 from antsibull_nox.utils import Version
 
 from ..utils import set_environ
+
+
+def test__LATEST_PYTHON_VERSION() -> None:
+    # Ensure that _LATEST_PYTHON_VERSION is at least as high as the latest Python version in CI
+    vt = (_LATEST_PYTHON_VERSION.major, _LATEST_PYTHON_VERSION.minor)
+    pvt = (sys.version_info.major, sys.version_info.minor)
+    assert vt >= pvt
 
 
 def fake_binary(path: Path, content: str) -> Path:
@@ -46,3 +54,14 @@ def test_get_installed_python_versions(tmp_path: Path) -> None:
             Version(2, 7): py27,
             Version(3, 5): py35,
         }
+
+
+def test_get_recent_python_version() -> None:
+    v30 = Version.parse("3.0")
+    v39 = Version.parse("3.9")
+    v310 = Version.parse("3.10")
+    next = _LATEST_PYTHON_VERSION.next_minor_version()
+    assert get_recent_python_version([v30, next]) == v30
+    assert get_recent_python_version([v30, v39, next]) == v39
+    assert get_recent_python_version([v30, v39, v310, next]) == v310
+    assert get_recent_python_version([next]) == next
